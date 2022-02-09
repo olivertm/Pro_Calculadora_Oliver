07e832fe8ff7829d1e78712673522969f91cae08
Oliver Tacoronte <olivertm@gmail.com>
2022-02-09 09:05:55 +0000

    añadida funcion porcentaje

--- src/main/java/com/mycompany/pro_calculadora_oliver/Calculadora.java
+++ src/main/java/com/mycompany/pro_calculadora_oliver/Calculadora.java
@@ -9,5 +9,13 @@ package com.mycompany.pro_calculadora_oliver;
  * @author olivert
  */
 public class Calculadora {
+    public static int porcentaje (int numero, int porcentaje) {
+        return (numero * porcentaje) / 100;
+    }
     
+    public static void main(String args[])
+    {
+        System.out.println("El 30% de 100 es : " + porcentaje(100, 30));
+    }
+
 }
