34b25c103e29377757ae83ef32c8e357e1c58097
Oliver Tacoronte <olivertm@gmail.com>
2022-02-09 08:41:07 +0000

    Añadimos las funciones suma y resta

--- src/main/java/com/mycompany/pro_calculadora_oliver/Calculadora.java
+++ src/main/java/com/mycompany/pro_calculadora_oliver/Calculadora.java
@@ -10,4 +10,18 @@ package com.mycompany.pro_calculadora_oliver;
  */
 public class Calculadora {
     
+    public static int Suma(int num1, int num2){
+        return num1+num2;
+    }
+    
+    public static int Resta(int num1, int num2){
+        return num1-num2;
+    }
+    
+    public static void main(String args[])
+    {
+        System.out.println("La suma de 3 + 1 = " + Suma(3,1));
+        System.out.println("La resta de 3 - 1 = " + Resta(3,1));
+    }
+    
 }
